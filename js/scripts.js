518693e2d9b641a047098a8553efa61bb4ce1f9a
Anna Gibbons and Marguerite Kennedy <marguerite@>
2019-04-01 16:05:49 -0700

    add an html page with js and css stylesheets, to attempt to use arrays and loops to pull a card from left and right.

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+$(document).ready.(function() {
+  $("#cards").click(function() {
+ $("#showCards").show();
+ $("#nocards").hide();
+ });
+ var lefts = ["2","3","4", "5", "6", "7", "8", "9", "10", "Jack", "Queen","King", "Ace"];
+ var rights = ["hearts", "spades", "diamonds", "clubs"];
+
+ var "showCards" = $(lefts.forEach(function(left) {
+   alert(left + " of " +right);
+ });
+
+//  var lefts = ["2","3","4", "5", "6", "7", "8", "9", "10", "Jack", "Queen","King", "Ace"];
+//  var rights = ["hearts", "spades", "diamonds", "clubs"];
+
+///  lefts.forEach(function(left) {
+    //alert(left + " of " +right);
+//  var left = lefts[Math.floor(Math.random() * lefts.length)];
+  //var right = rights[Math.floor(Math.random() * rights.length)];
+  });
+});
